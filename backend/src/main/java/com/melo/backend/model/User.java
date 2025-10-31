330658178052057ad3fa000e7e06bf91e1138437
ryan-de-melo <jos.ryan.melo@gmail.com>
2025-10-31 07:16:05 -0300

    add: User model

---
+++ backend/src/main/java/com/melo/backend/model/User.java
@@ -0,0 +1,49 @@
+package com.melo.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @NotEmpty
+    @NotNull
+    @Size(max = 63)
+    private String name;
+
+    @Email
+    @NotEmpty
+    @NotNull
+    @Size(max = 63)
+    private String email;
+
+    @NotEmpty
+    @NotNull
+    @NotBlank
+    @Size(min = 8)
+    private transient String password;
+
+}
