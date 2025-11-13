8f0dd8fc2d9d3487a292e68fb91c98692b75c2f4
José Melo <jos.ryan.melo@gmail.com>
2025-11-13 20:16:21 -0300

    .

--- database/init.sql
+++ database/init.sql
@@ -1,1 +1,8 @@
 -- POSTGRESQL Initialization
+
+CREATE TABLE users (
+    id                BIGSERIAL PRIMARY KEY,
+    name              VARCHAR(255) NOT NULL,
+    email             VARCHAR(255) NOT NULL UNIQUE,
+    encoded_password  VARCHAR(255) NOT NULL
+);
