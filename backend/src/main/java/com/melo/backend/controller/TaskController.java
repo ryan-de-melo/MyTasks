406ac5609689c5ca411a0e27df2c643c820a3dab
José Melo <jos.ryan.melo@gmail.com>
2025-12-30 16:16:16 -0300

    chore: allow cors from localhost:5173

--- backend/src/main/java/com/melo/backend/controller/TaskController.java
+++ backend/src/main/java/com/melo/backend/controller/TaskController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -18,6 +19,7 @@ import com.melo.backend.infrastructure.dto.task.TaskAddDTO;
 import com.melo.backend.infrastructure.dto.task.TaskResponseDTO;
 import com.melo.backend.infrastructure.dto.task.TaskUpdateDTO;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
