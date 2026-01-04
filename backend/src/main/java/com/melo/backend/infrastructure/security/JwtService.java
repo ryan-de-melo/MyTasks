941e381744a00b25656d77c4c61ab3286255cf0c
José Melo <jos.ryan.melo@gmail.com>
2026-01-04 17:01:04 -0300

    feat: add generate token in jwt service

--- backend/src/main/java/com/melo/backend/infrastructure/security/JwtService.java
+++ backend/src/main/java/com/melo/backend/infrastructure/security/JwtService.java
@@ -1,5 +1,24 @@
 package com.melo.backend.infrastructure.security;
 
+import java.util.Date;
+
+import com.melo.backend.infrastructure.model.User;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
 public class JwtService {
+
+    public final String SECRET = "my-tasks-is-the-best-task-manager-out-there";  // this secret is for study purposes only
+
+    public String generateToken(User user) {
+        return Jwts.builder()
+                    .setSubject(user.getEmail())
+                    .claim("userId", user.getId())
+                    .setIssuedAt(new Date())
+                    .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                    .signWith(Keys.hmacShaKeyFor(SECRET.getBytes()))
+                    .compact();
+    }
 
 }
