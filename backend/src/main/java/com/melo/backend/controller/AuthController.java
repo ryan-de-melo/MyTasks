f5e5c40ada3b6e1b63f2c39f1fc2fef654299801
José Melo <jos.ryan.melo@gmail.com>
2026-01-04 17:20:12 -0300

    feat: auth controller with register and login

---
+++ backend/src/main/java/com/melo/backend/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.melo.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.melo.backend.business.AuthService;
+import com.melo.backend.infrastructure.dto.auth.AuthResponseDTO;
+import com.melo.backend.infrastructure.dto.user.UserLoginDTO;
+import com.melo.backend.infrastructure.dto.user.UserRegisterDTO;
+
+import jakarta.validation.Valid;
+
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    public final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    public ResponseEntity<Void> register(@RequestBody @Valid UserRegisterDTO dto) {
+        authService.register(dto);
+        return ResponseEntity.ok().build();
+    }
+
+    public ResponseEntity<AuthResponseDTO> login(@RequestBody @Valid UserLoginDTO dto) {
+        return ResponseEntity.ok(authService.login(dto));
+    }
+
+}
