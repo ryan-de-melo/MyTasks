a149579a04c915cd185b3ca5243cf6db059265c3
José Melo <jos.ryan.melo@gmail.com>
2026-01-13 16:32:17 -0300

    fix(AuthenticatedUserService): reimplemented with UserDetails

--- backend/src/main/java/com/melo/backend/service/AuthenticatedUserService.java
+++ backend/src/main/java/com/melo/backend/service/AuthenticatedUserService.java
@@ -1,23 +1,18 @@
 package com.melo.backend.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import com.melo.backend.entity.User;
-import com.melo.backend.repository.UserRepository;
+import com.melo.backend.entity.UserDetailsImpl;
 
 @Component
 public class AuthenticatedUserService {
 
-    @Autowired
-    private UserRepository repo;
-
     public User get() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        String email = auth.getName();
-
-        return repo.findByEmail(email).orElseThrow(() -> new RuntimeException("Not found"));
+        UserDetailsImpl principal = (UserDetailsImpl) auth.getPrincipal();
+        return principal.getUser();
     }
 }
