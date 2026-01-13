763eb55c29a661db0ce5863a267d7979c70d14a3
José Melo <jos.ryan.melo@gmail.com>
2026-01-13 16:32:47 -0300

    feat: UserDetails implementation

---
+++ backend/src/main/java/com/melo/backend/entity/UserDetailsImpl.java
@@ -0,0 +1,36 @@
+package com.melo.backend.entity;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    public UserDetailsImpl(User user) {
+        this.user = user;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+}
