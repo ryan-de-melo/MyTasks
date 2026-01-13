67f248a90b9531f127cb557265d09040e669fdd9
José Melo <jos.ryan.melo@gmail.com>
2026-01-13 10:00:39 -0300

    test(user service): update by id

--- backend/src/test/java/com/melo/backend/serviceTest/UserServiceTest.java
+++ backend/src/test/java/com/melo/backend/serviceTest/UserServiceTest.java
@@ -7,14 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.melo.backend.dto.user.UserRegisterDTO;
 import com.melo.backend.dto.user.UserResponseDTO;
+import com.melo.backend.dto.user.UserUpdateDTO;
 import com.melo.backend.entity.User;
 import com.melo.backend.repository.UserRepository;
 import com.melo.backend.service.UserService;
@@ -96,5 +99,27 @@ public class UserServiceTest {
         UserResponseDTO response = service.deleteById(Long.valueOf(0));
 
         assertEquals("test@email.com", response.email());
+    }
+
+    @Test
+    void testUpdateById() {
+        User usr = User.builder()
+                .id(Long.valueOf(0))
+                .name("test")
+                .email("test@email.com")
+                .password("testpassword")
+                .build();
+
+        when(repo.findById(Long.valueOf(0))).thenReturn(Optional.of(usr));
+        when(repo.save(any(User.class))).thenReturn(usr);
+
+        UserUpdateDTO dto = new UserUpdateDTO("newtest", "newtestpassword");
+
+        service.updateById(Long.valueOf(0), dto);
+
+        verify(repo).save(argThat(user ->
+                                    user.getName().equals("newtest") &&
+                                    user.getPassword().equals("newtestpassword")
+    ));
     }
 }
