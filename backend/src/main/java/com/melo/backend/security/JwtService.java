907a90408696182d6512fce43df745558f0c210c
José Melo <jos.ryan.melo@gmail.com>
2026-01-21 19:16:12 -0300

    refactor: update JwtService to use external secret and simplify token methods

--- backend/src/main/java/com/melo/backend/security/JwtService.java
+++ backend/src/main/java/com/melo/backend/security/JwtService.java
@@ -1,55 +1,22 @@
 package com.melo.backend.security;
 
-import java.util.Date;
-import java.util.function.Function;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.melo.backend.entity.User;
 
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
-
 @Service
 public class JwtService {
 
-    public final String SECRET = "my-tasks-is-the-best-task-manager-out-there";  // this secret is for study purposes only
+    @Value("${api.security.token.secret}")
+    public String SECRET;
 
     public String generateToken(User user) {
-        return Jwts.builder()
-                    .setSubject(user.getEmail())
-                    .claim("userId", user.getId())
-                    .setIssuedAt(new Date())
-                    .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-                    .signWith(Keys.hmacShaKeyFor(SECRET.getBytes()))
-                    .compact();
-    }
-
-    public String extractEmail(String token) {
-        return extractClaim(token, Claims::getSubject);
+        return null;
     }
 
     public boolean isTokenValid(String token) {
-        try {
-            Jwts.parserBuilder()
-                .setSigningKey(Keys.hmacShaKeyFor(SECRET.getBytes()))
-                .build()
-                .parseClaimsJws(token);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
-
-    private <T> T extractClaim(String token, Function<Claims, T> solver) {
-        Claims claims = Jwts.parserBuilder()
-                            .setSigningKey(Keys.hmacShaKeyFor(SECRET.getBytes()))
-                            .build()
-                            .parseClaimsJws(token)
-                            .getBody();
-
-        return solver.apply(claims);
+        return true;
     }
 
 }
