636a999853774d43cb7b7879819c6bfcdb8e988f
José Melo <jos.ryan.melo@gmail.com>
2026-01-22 16:29:21 -0300

    feat: add new SecurityFilter

---
+++ backend/src/main/java/com/melo/backend/security/SecurityFilter.java
@@ -0,0 +1,62 @@
+package com.melo.backend.security;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.melo.backend.repository.UserRepository;
+import com.melo.backend.service.TokenService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String token = recoverToken(request);
+        String email = tokenService.validateToken(token);
+
+        if (token != null && email != null) {
+            UserDetails user = userRepository.findByEmail(email);
+
+            if (user != null ) {
+                UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+                    user, 
+                    null, 
+                    user.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String authHeader = request.getHeader("Authorization");
+        String token = null;
+
+        if (authHeader != null) {
+            token = authHeader.replace("Bearer ", "");
+        }
+
+        return token;
+    }
+
+    
+}
