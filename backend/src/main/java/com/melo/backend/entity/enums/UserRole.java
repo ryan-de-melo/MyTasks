32a520c0b9b2e3f844cba9aaedae889260447cd6
José Melo <jos.ryan.melo@gmail.com>
2026-01-26 21:43:02 -0300

    refactor: simplification of role type

--- backend/src/main/java/com/melo/backend/entity/enums/UserRole.java
+++ backend/src/main/java/com/melo/backend/entity/enums/UserRole.java
@@ -1,17 +1,5 @@
 package com.melo.backend.entity.enums;
 
 public enum UserRole {
-    ADMIN("admin"),
-    USER("user");
-
-    @SuppressWarnings("FieldMayBeFinal")
-    private String role;
-
-    UserRole(String role) {
-        this.role = role;
-    }
-
-    public String getRole() {
-        return this.role;
-    }
+    ADMIN, USER
 }
