dd855d6f2de642a49f6e41b56f5dd249c710764b
José Melo <jos.ryan.melo@gmail.com>
2026-01-27 13:48:38 -0300

    fix: change return type

--- backend/src/main/java/com/melo/backend/repository/UserRepository.java
+++ backend/src/main/java/com/melo/backend/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.melo.backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -9,5 +11,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     // Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
-    UserDetails findByEmail(String email);
+    Optional<UserDetails> findByEmail(String email);
 }
