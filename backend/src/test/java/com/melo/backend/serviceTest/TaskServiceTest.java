9ee087960c8e728aec636f8c861ad5daded94b9c
José Melo <jos.ryan.melo@gmail.com>
2026-01-29 22:00:36 -0300

    test: delete task and delete with error

--- backend/src/test/java/com/melo/backend/serviceTest/TaskServiceTest.java
+++ backend/src/test/java/com/melo/backend/serviceTest/TaskServiceTest.java
@@ -2,12 +2,16 @@ package com.melo.backend.serviceTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -22,6 +26,7 @@ import com.melo.backend.entity.Task;
 import com.melo.backend.entity.User;
 import com.melo.backend.entity.enums.TaskPriority;
 import com.melo.backend.entity.enums.TaskStatus;
+import com.melo.backend.exception.TaskNotFoundException;
 import com.melo.backend.repository.TaskRepository;
 import com.melo.backend.service.AuthenticationService;
 import com.melo.backend.service.TaskService;
@@ -38,15 +43,29 @@ public class TaskServiceTest {
     @InjectMocks
     private TaskService service;
 
-    private User userDefault;
+    private static User userDefault;
+
+    private static Task taskDefault;
 
     @BeforeAll
-    public void setUp() {
+    public static void setUp() {
         userDefault = User.builder()
                 .id(Long.valueOf(0))
                 .email("test@email.com")
                 .name("test")
                 .password("testpassword")
+                .build();
+
+        taskDefault = Task.builder()
+                .id(1L)
+                .title("Teste")
+                .description("testando")
+                .priority(TaskPriority.HIGH)
+                .status(TaskStatus.DOING)
+                .deadline(LocalDateTime.now().plusDays(2).toInstant(ZoneOffset.of("-03:00")))
+                .user(userDefault)
+                .createdAt(Instant.now())
+                .updatedAt(Instant.now())
                 .build();
     }
 
@@ -95,22 +114,24 @@ public class TaskServiceTest {
         assertEquals(dto.title(), response.title());
     }
 
-    // TODO terminar
     @Test
     void testDeleteTask() {
-        Task savedTask = Task.builder()
-                .id(1L)
-                .title("Teste")
-                .description("testando")
-                .priority(TaskPriority.HIGH)
-                .status(TaskStatus.DOING)
-                .deadline(LocalDateTime.now().plusDays(2).toInstant(ZoneOffset.of("-03:00")))
-                .user(userDefault)
-                .createdAt(Instant.now())
-                .updatedAt(Instant.now())
-                .build();
+        when(authService.getCurrentUser()).thenReturn(userDefault);
+        when(repository.findByIdAndUser(taskDefault.getId(), userDefault)).thenReturn(Optional.of(taskDefault));
 
-        
+        service.deleteById(1L);
+
+        verify(repository, times(1)).deleteById(taskDefault.getId());
+    }
+
+    @Test
+    void testDeleteError() {
+        when(authService.getCurrentUser()).thenReturn(userDefault);
+        when(repository.findByIdAndUser(taskDefault.getId(), userDefault)).thenThrow(TaskNotFoundException.class);
+
+        assertThrows(TaskNotFoundException.class, () -> service.deleteById(taskDefault.getId()));
+
+        verify(repository, times(0)).deleteById(taskDefault.getId());
     }
 
 }
