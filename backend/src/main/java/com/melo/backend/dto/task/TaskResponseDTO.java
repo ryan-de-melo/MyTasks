a1144d7212bd40d92ec9621a505d0d8f323f87d2
José Melo <jos.ryan.melo@gmail.com>
2026-01-31 21:09:44 -0300

    fix: add id to dto

--- backend/src/main/java/com/melo/backend/dto/task/TaskResponseDTO.java
+++ backend/src/main/java/com/melo/backend/dto/task/TaskResponseDTO.java
@@ -9,6 +9,7 @@ import com.melo.backend.entity.enums.TaskStatus;
 import com.melo.backend.mappers.UserMapper;
 
 public record TaskResponseDTO(
+    Long id,
     String title,
     String description,
     TaskPriority priority,
@@ -19,13 +20,16 @@ public record TaskResponseDTO(
     UserResponseDTO user
 ) {
     public TaskResponseDTO(Task task) {
-        this(task.getTitle(),
+        this(
+            task.getId(),
+            task.getTitle(),
             task.getDescription(),
             task.getPriority(),
             task.getStatus(),
             task.getCreatedAt(),
             task.getUpdatedAt(),
             task.getDeadline(),
-            UserMapper.toResponse(task.getUser()));
+            UserMapper.toResponse(task.getUser())
+        );
     }
 }
